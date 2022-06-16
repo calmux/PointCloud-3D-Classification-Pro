c75b3738bf29cbb2ee8c75eb84f4f61ebce6daae
calmux <calmuxoto@proton.me>
2022-06-16 12:43:46 +0000

    Run scan-local.sh on jenkins:...

--- lib/Python_wrapper.h
+++ lib/Python_wrapper.h
@@ -2,4 +2,8 @@
 //" /usr/include/python3.6m/object.h:448:23: error: expected unqualified-id before ‘;’ token 
 //PyType_Slot *slots; /* terminated by slot==0. */"
 //more info: https://stackoverflow.com/questions/23068700/embedding-python3-in-qt-5
-//USAGE: I
+//USAGE: In Qt project replace every "#include <Python.h>" with "include <Python_wrapper.h>" 
+#pragma push_macro("slots")
+#undef slots
+#include "Python.h"
+#pragma pop_macro("slots")
