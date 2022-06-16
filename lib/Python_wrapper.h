8333f82236e1eda071a2e7e7bf3ae37b75d8a6c0
calmux <calmuxoto@proton.me>
2022-06-16 09:45:33 +0000

    doxygen fixes

---
+++ lib/Python_wrapper.h
@@ -0,0 +1,5 @@
+//USE ONLY IN Qt projects (solves problem:
+//" /usr/include/python3.6m/object.h:448:23: error: expected unqualified-id before ‘;’ token 
+//PyType_Slot *slots; /* terminated by slot==0. */"
+//more info: https://stackoverflow.com/questions/23068700/embedding-python3-in-qt-5
+//USAGE: I
