79fb9877bf382a834f1bf51f4a23d5736cdf2aad
calmux <calmuxoto@proton.me>
2022-06-30 07:05:05 +0000

    Regenerate gemspec for version 0.5.2

--- src/controller/pointCloudController.cpp
+++ src/controller/pointCloudController.cpp
@@ -5,4 +5,15 @@ PointCloudController::PointCloudController()
 {
     boundingBox= std::make_shared<AxisAlignedBoudningBox>();
     visualization= std::make_shared<VisualizationWithBB>(boundingBox);
-    objectFactory= std
+    objectFactory= std::make_shared<CloudObjectFactory>();
+    sceneFactory=std::make_shared<CloudSceneFactory>();
+    saveLoad = std::make_shared<SaveLoad>();
+    saveLoad->setFactories(this->sceneFactory,this->objectFactory);
+}
+void PointCloudController::registerQTreeWidgetObserver(QTreeWidget *treeWiOb){
+    this->treeWidgetObserver=treeWiOb;
+}
+void PointCloudController::registerQVTKidgetObserver(QVTKWidget *qVTKWiOb){
+    this->qVTKWidgetObserver=qVTKWiOb;
+}
+void PointClou
