a5ba2cde1c02e2a531e8c09657028ae9a8401895
calmux <calmuxoto@proton.me>
2022-10-22 11:56:40 +0000

    Changed folder name

--- src/controller/pointCloudController.h
+++ src/controller/pointCloudController.h
@@ -4,4 +4,19 @@
 
 #include "src/model/cloudFactory.h"
 class MainWindow;
-#include "src
+#include "src/view/mainwindow.h"
+
+class CloudComponent;
+class CloudScene;
+class CloudObject;
+#include "src/model/pointCloud.h"
+class SaveLoad;
+#include "src/view/saveLoad.h"
+#include <limits>
+
+class PointCloudController: public std::enable_shared_from_this<PointCloudController> //#//https://stackoverflow.com/questions/27894246/cast-this-to-stdshared-ptr
+{
+private:
+    std::vector<std::unique_ptr<CloudComponent>> cachedClouds;
+    std::shared_ptr<CloudSceneFactory> sceneFactory;
+    std
