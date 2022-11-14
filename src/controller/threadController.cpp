e156f7d92d9d95241ddf4ae2999b0b90b719d42e
calmux <calmuxoto@proton.me>
2022-11-14 07:19:06 +0000

    Add user model

--- src/controller/threadController.cpp
+++ src/controller/threadController.cpp
@@ -1,3 +1,11 @@
 #include "src/controller/threadController.h"
 /* stara wersja classify
-void Worker::classify(const cloudControllerSharedPtr modelController, listOfTreeWidgetItemRawPtr selectedClouds,classificationTypeSharedPtr cla
+void Worker::classify(const cloudControllerSharedPtr modelController, listOfTreeWidgetItemRawPtr selectedClouds,classificationTypeSharedPtr classificationType,stdString modelPath,integer numOfClasses){
+    QString result="CLASSIFICATION FINISHED";
+    //qDebug()<<"CLASSIFICATION thread:  "<<thread()->currentThreadId();
+    modelController->classify(selectedClouds,classificationType,modelPath,numOfClasses);
+    emit resultReady(result);
+}
+*/
+void Worker::classify(const cloudControllerSharedPtr modelController, listOfTreeWidgetItemRawPtr selectedClouds
+              ,classificati
