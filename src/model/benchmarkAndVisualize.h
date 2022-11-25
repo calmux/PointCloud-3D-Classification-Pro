22a77c9d9b1858676942bab0731069d624815024
calmux <calmuxoto@proton.me>
2022-11-25 12:52:39 +0000

    SCSI: fix scsi_wait_scan

--- src/model/benchmarkAndVisualize.h
+++ src/model/benchmarkAndVisualize.h
@@ -11,4 +11,14 @@
 #include <fstream> //->?do zapisywania do pliku .txt
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
-enum BBType{type_AxisAlignedBoundin
+enum BBType{type_AxisAlignedBoundingBox, type_OrientedBoundingBox, type_NotSpecified};
+
+class BoundingBoxType{
+protected:
+    pcl::PointXYZ min_point;
+    pcl::PointXYZ max_point;
+    pcl::PointXYZ center; //->? ustaw paramtery domyslne ! np. #include <limits> + center=std::numeric_limits<float>::quiet_NaN();
+    float l=-1; //object (or max bounding box when using AABB) length
+    float w=-1; //
+    float h=-1;
+    float yaw;//->? ustaw paramtery domyslne ! np. #include <limits> + center=s
