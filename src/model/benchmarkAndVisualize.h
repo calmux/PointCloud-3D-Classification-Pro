0feb2edc2a55a7028710ab4ed5d8521c1e361989
calmux <calmuxoto@proton.me>
2022-11-25 08:01:04 +0000

    Bump acorn from 6.2.1 to 6.4.1

--- src/model/benchmarkAndVisualize.h
+++ src/model/benchmarkAndVisualize.h
@@ -1,2 +1,14 @@
 //
-// Creat
+// Created by radek on 21.03.19.
+//
+
+#ifndef INC_3DPOINTCLOUDCLASSIFICATION_BENCHMARKANDVISUALIZE_H
+#define INC_3DPOINTCLOUDCLASSIFICATION_BENCHMARKANDVISUALIZE_H
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/features/moment_of_inertia_estimation.h>
+#include <pcl/common/common.h>//do getMinMax3D
+#include <fstream> //->?do zapisywania do pliku .txt
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+enum BBType{type_AxisAlignedBoundin
