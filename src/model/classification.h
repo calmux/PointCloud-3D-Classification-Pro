02d652027b4f35713d70131724355629f59c3d89
calmux <calmuxoto@proton.me>
2022-12-04 16:58:16 +0000

    still testing

--- src/model/classification.h
+++ src/model/classification.h
@@ -5,4 +5,23 @@
 #ifndef INC_3DPOINTCLOUDCLASSIFICATION_CLASSIFICATION_H
 #define INC_3DPOINTCLOUDCLASSIFICATION_CLASSIFICATION_H
 
-#include "lib/pyboos
+#include "lib/pyboostcvconverter/pyboostcvconverter.hpp"
+
+//For python embedding:
+#if (PY_VERSION_HEX >= 0x03000000)
+
+static void *init_ar() {
+#else
+    static void init_ar(){
+#endif
+    Py_Initialize();
+
+    import_array();
+    return NUMPY_IMPORT_ARRAY_RETVAL;
+}
+
+class ClassificationType{
+public:
+    virtual std::vector<float> classifyAndGetNNResponeVector(const std::vector<cv::Mat>& projection_vector, const std::string modelDirectory, int numOfClasses)=0;
+};
+class MultiViewClassification:public 
