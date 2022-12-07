d40213d358f03198b30511cafbb11c06d3ccde62
calmux <calmuxoto@proton.me>
2022-12-07 07:52:01 +0000

    Auto commit:  M .bash_aliases

--- src/model/edit.cpp
+++ src/model/edit.cpp
@@ -7,4 +7,11 @@ ProjectionEdit::ProjectionEdit(cv::MorphShapes kerShape,uint kerWidth, uint kerH
 void ProjectionEdit::edit(std::vector<cv::Mat> &projections){
      //photo editing
      cv::Mat kernel= cv::getStructuringElement(kernelShape,cv::Size(kernelWidth,kernelHeight)); //bez upsampling stosowalem 6,6  //po upsampling 3,3
-     ///Canny( vecMat, vec
+     ///Canny( vecMat, vecMat, 100, 200);
+     std::vector<int> compression_params;
+     compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+     compression_params.push_back(100);
+     //store image in memory buffer (no need to write to file)
+     std::vector<uchar> buf;
+     //UWAGA - wynik klasyfikacji nieznacznie sie rozni w zaleznosci od zastosowania ponizej jedenj z 3 operacji:
+     //1. Zakodowanie (z domyslnymi parametrami czyli m.in. CV_IMWRITE_JPEG_QUALITY=0.95)  orginalnej powyzszej
