228959167dc19b376ff7994ea048df9b8542a308
calmux <calmuxoto@proton.me>
2023-02-27 14:53:04 +0000

    Fix template name

--- src/model/pointCloud.h
+++ src/model/pointCloud.h
@@ -17,4 +17,21 @@
 #include "src/model/cloudFactory.h"
 #include "src/model/segmentation.h"
 #include "src/model/classification.h"
-#include 
+#include "src/model/benchmarkAndVisualize.h"
+#include "src/model/edit.h"
+class PointCloudController;
+#include "src/controller/pointCloudController.h"
+
+class CloudObjectFactory;
+class CloudSceneFacory;
+
+enum cloudType{type_CloudScene, type_CloudObject};
+
+class CloudComponent{
+protected:
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+    cloudType type;
+    std::shared_ptr<BenchmarkType> benchmarkType=nullptr;
+    std::string name="default name";
+    std::string sourcePath="";
+    std::string
