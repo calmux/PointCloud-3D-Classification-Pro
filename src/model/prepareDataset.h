c7c2bdd419187126eff2a86575301b9df461adba
calmux <calmuxoto@proton.me>
2023-03-21 10:49:43 +0000

    Added metadata

--- src/model/prepareDataset.h
+++ src/model/prepareDataset.h
@@ -26,4 +26,17 @@ protected:
     std::vector<std::string> classesOfInterest={};
     std::vector<std::tuple<std::string,std::string,std::string>> getPathAndNameAndParentFolderName(const std::string extension=".pcd",bool removeExtensionFromName=true);
 };
-class
+class PrepareDatasetFrom3D:public PrepareDataset{
+protected:
+    std::shared_ptr<CloudObject> object=nullptr;
+    std::shared_ptr<CloudObjectFactory> factory=nullptr;
+    std::shared_ptr<ProjectionType> projection=nullptr;
+    std::vector<std::string> fnames; //fnames of 3D objects
+    uint TOTAL_PROJECTIONS;
+    uint default_num_of_proj_per_obj=36;
+    double START_DEGREE=0;
+    double END_DEGREE=360;
+    double START_v=0;
+    double END_v=0;
+    int N_v=1;
+    //bool RANGE_I
