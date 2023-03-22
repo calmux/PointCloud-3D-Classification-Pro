bb8fe420aca928d60e775268a7662c42be6b38cc
calmux <calmuxoto@proton.me>
2023-03-22 21:11:50 +0000

    Updating to match refinement syntax

--- src/model/projection.cpp
+++ src/model/projection.cpp
@@ -1,2 +1,14 @@
 //
 // Created by radek on 15.03.19.
+//
+#include "src/model/projection.h"
+
+#include <pcl/common/common.h>//do getMinMax3D
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/common/eigen.h>//do uzyskania macierzy transformacji (bool pcl::transformBetween2CoordinateSystems)
+#include <pcl/common/transforms.h> //do transformacji chmury ( pcl::transformPointCloud) z wykorzystaniem macierzy transformacji
+#include <pcl/visualization/range_image_visualizer.h>
+#include <cmath>
+
+#define PI 3.14159265
