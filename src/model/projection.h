59a4fc1f768daedb11bbe12a85d214578471127b
calmux <calmuxoto@proton.me>
2023-09-01 17:32:27 +0000

    tracing: Fix ftrace_dump()

--- src/model/projection.h
+++ src/model/projection.h
@@ -2,4 +2,13 @@
 // Created by radek on 15.03.19.
 //
 
-#ifndef 
+#ifndef INC_3DPOINTCLOUDCLASSIFICATION_PROJECTION_H
+#define INC_3DPOINTCLOUDCLASSIFICATION_PROJECTION_H
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <opencv2/opencv.hpp>
+#include <pcl/range_image/range_image_planar.h>
+#include <string>
+struct ProjectionParameters{ //# NAUKA-> CZYTAJ: https://stackoverflow.com/questions/4203010/how-to-initialize-member-struct-in-initializer-list-of-c-class
+    ProjectionParameters(double min_deg,double max_deg , int n, double min_h, double max_h,int n_h
