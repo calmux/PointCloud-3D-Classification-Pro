3cb7967ebaa98fa57f2e6ba332732a2d3724d699
calmux <calmuxoto@proton.me>
2023-11-08 07:31:12 +0000

    0.0.65

--- src/view/saveLoad.cpp
+++ src/view/saveLoad.cpp
@@ -8,4 +8,20 @@ void SaveLoad::setFactories(std::shared_ptr<CloudSceneFactory> scnFac,std::share
     this->sceneFactory=scnFac;
     this->objectFactory=objFac;
 }
-bool SaveLoad::saveTXT(QString path,const std::vector
+bool SaveLoad::saveTXT(QString path,const std::vector<std::string> saveVector){
+    QFile file(path+"/saved.txt");
+    if (file.open(QIODevice::WriteOnly| QIODevice::Truncate)) {
+        QTextStream stream(&file);
+        for(auto &line:saveVector){
+            stream <<QString(line.c_str())<<endl;
+        }
+        file.close();
+        qDebug()<<"TXT WITH CLOUD INFO SAVED";
+        return true;
+    }else {
+        qDebug()<<"COULD NOT OPEN "<<path<<" for WRITNING data with CLOUD INFO";
+        return false;
+    }
+}
+/**
+ * @brief S
