189af84c76d2e8ed2e48a17c1c848f3cb59508ca
calmux <calmuxoto@proton.me>
2023-12-29 22:10:32 +0000

    fix issue #3

--- src/view/saveSettings.cpp
+++ src/view/saveSettings.cpp
@@ -24,3 +24,11 @@ void SaveSettings::on_pushButton_2_clicked()
 void SaveSettings::on_save_pushButton_clicked()
 {
     savePath=QFileDialog::getExistingDirectory(this,tr("Select clouds save folder"),"/home/");
+    if(this->ui->clsIDs_lineEdit->text()!=""){
+        addCondition(std::bind(&SaveSettings::isAmongClasses, this, std::placeholders::_1));
+    }
+    if(this->ui->nnRespon_doubleSpinBox->value()>0){
+        addCondition(std::bind(&SaveSettings::hasNnResponseStrengthBiggerEqualThan, this, std::placeholders::_1));
+    }
+    if(this->ui->cloudSize_spinBox->value()>0){
+        addCondition(std::bind(&SaveSettings::hasNumOfPointsBiggerE
