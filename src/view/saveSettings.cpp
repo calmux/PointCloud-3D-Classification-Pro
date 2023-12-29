30f0e4b70fb0be2a04c8bc8153a87f69ed8d94da
calmux <calmuxoto@proton.me>
2023-12-29 22:00:14 +0000

    [Changelog]

--- src/view/saveSettings.cpp
+++ src/view/saveSettings.cpp
@@ -1,4 +1,26 @@
 #include "src/view/saveSettings.h"
 #include "ui_saveSettings.h"
 
-SaveSettings:
+SaveSettings::SaveSettings(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SaveSettings)
+{
+    ui->setupUi(this);
+    saveConditions=std::make_shared<SaveConditions>();
+}
+
+SaveSettings::~SaveSettings()
+{
+    delete ui;
+}
+QString SaveSettings::getSavePath(){
+    return this->savePath;
+}
+void SaveSettings::on_pushButton_2_clicked()
+{
+     reject();
+}
+
+void SaveSettings::on_save_pushButton_clicked()
+{
+    savePath=QFileDialog::getExistingDirectory(this,tr("Select clouds save folder"),"/home/");
